a306b2f5ffa6229090e90df81eb1514c2f13d8df
siddh34 <ssiddharthsutar@gmail.com>
2023-01-01 16:58:41 +0530

    "Added firebase"

--- lib/Screens/UserScreen.dart
+++ lib/Screens/UserScreen.dart
@@ -4,6 +4,7 @@ class UserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
       body: Column(
         children: [
           Text(
