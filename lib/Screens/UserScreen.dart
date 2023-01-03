abad54b2e4e371661443e363458583f0042fd849
siddh34 <ssiddharthsutar@gmail.com>
2023-01-03 20:49:08 +0530

    "Fetched Data"

--- lib/Screens/UserScreen.dart
+++ lib/Screens/UserScreen.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:myirrigation/Utilities/DataManager.dart';
 
-class UserScreen extends StatelessWidget {
+
+class UserScreen extends StatefulWidget {
+  @override
+  State<UserScreen> createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  // functions
+  DataManager myDataManager = new DataManager();
+
+  @override
+  void initState() async {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 255, 255, 255),
-      body: Column(
-        children: [
-          Text(
-            'Hello user!',
-          )
-        ],
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(140),
+              child: TextButton(
+                  onPressed: (() {
+                    myDataManager.getData();
+                  }),
+                  child: Text('Press')),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/Utilities/DataManager.dart
+++ lib/Utilities/DataManager.dart
@@ -1,4 +1,25 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-class DataManager{
-  
+class DataManager {
+  // function or variables
+
+  Future<void> getData() async {
+    try {
+      await Firebase.initializeApp();
+      
+      DatabaseReference reference = FirebaseDatabase.instance.ref();
+
+      var snapshot = await reference.get();
+
+      if (snapshot.exists) {
+        print(snapshot.value);
+      }
+      else{
+        print("Data not found");
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 }
